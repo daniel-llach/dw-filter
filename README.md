d0168a21148c7dd1e55af742361c95327a7701ab
daniel-llach <daniel.llach@gmail.com>
2016-03-11 16:10:27 -0300

    live example

---
+++ README.md
@@ -0,0 +1,264 @@
+# dw-filter (under construction...)
+A filter component for **darwinEd©** app of **Foris**.
+
+<img src="https://github.com/daniel-llach/dw-filter/blob/master/img/dw-filter.png?raw=true">
+
+# Index:
+
+<a href="#1--install">1.- Install</a> </br>
+<a href="#2--use">2.- Use</a> </br>
+<a href="#3--api">3.- API</a> </br>
+&nbsp;&nbsp;&nbsp;&nbsp;<a href="#31--init">3.1.- Init</a> </br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#311--title">3.1.1.- Title</a> </br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#312--type">3.1.2.- Type</a> </br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#313--search">3.1.3.- Search</a> </br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#314--height">3.1.4.- Height</a> </br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#315--config">3.1.5.- Config</a> </br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#316--data">3.1.6.- Data</a> </br>
+&nbsp;&nbsp;&nbsp;&nbsp;<a href="#32--val">3.2.- Val</a> </br>
+&nbsp;&nbsp;&nbsp;&nbsp;<a href="#33--destroy">3.3.- Destroy</a> </br>
+<a href="#4--listeners">4.- Listeners</a> </br>
+<a href="#5--demo">5.- Demo</a> </br>
+<a href="#6--possible-problems">6.- Possible problems</a> </br>
+
+# 1.- Install
+1.1.- Install dependencies from bower into your project
+```javascript
+  bower install --save dw-filter
+```
+1.2.- Include dependencies in your html:
+```html
+<!-- dw-filter dependencies -->
+<script src="./bower_components/jquery/dist/jquery.min.js"></script>
+<script src="./component/dw-filter.js"></script>
+<link rel="stylesheet" type="text/css" href="./component/dw-filter.css">
+```
+
+# 2.- Use
+Execute the dwFilter class on a selector. It will be rendered a dw-filter element in this container inherit its position and width.
+```javascript
+$('#id').dwFilter();
+```
+
+# 3.- API
+## 3.1.- Init
+If the dwFilter() class has an object the API interprets that is a new element and create it.
+
+The API accepts the next configurations:
+
+### 3.1.1.- title
+### 3.1.2.- type
+
+· **checkbox**: List of checkboxes
+
+<img src="https://github.com/daniel-llach/dw-filter/blob/master/img/checkbox.png?raw=true">
+
+An example:
+```javascript
+$('#sample1').dwSelect({
+  data: [
+    {
+      id: 1,
+      primary: 'Cat',
+      secundary: 'Gray',
+      selected: false,
+      group: ['feline']
+    },
+    {
+      id: 2,
+      primary: 'Dog',
+      secundary: 'White',
+      selected: false,
+      group: ['canine']
+    },
+  ]
+});
+```
+· **selectChain** List of selects
+
+<img src="https://github.com/daniel-llach/dw-filter/blob/master/img/checkbox.png?raw=true">
+
+An example:
+```javascript
+$('#sample2').dwFilter({
+  title: 'title2',
+  type: 'selectChain',
+  search: 'outer',
+  config: {
+    key_attr: 'id',
+    name_attr: 'name',
+    value_attr: 'content',
+    options_key_attr: 'id',
+    options_value_attr: 'content',
+    options_active_attr: 'selected'
+  },
+  data: [
+    {
+      id: 1555,
+      name: 'The select',
+      content: [
+        {
+          id: 4565,
+          content: 'The option 1',
+          selected: false
+        },
+        {
+          id: 1212,
+          content: 'The option 2',
+          selected: true
+        }
+      ]
+    },
+    {
+      id: 3434,
+      name: 'Super select',
+      content: [
+        {
+          id: 4565,
+          content: 'Super option 1',
+          selected: false
+        },
+        {
+          id: 1212,
+          content: 'Super option 2',
+          selected: false
+        }
+      ]
+    },
+    {
+      id: 2211,
+      name: 'What a select',
+      content: [
+        {
+          id: 4565,
+          content: 'What option 1',
+          selected: false
+        },
+        {
+          id: 1212,
+          content: 'What option 2',
+          selected: true
+        }
+      ]
+    },
+    {
+      id: 4535,
+      name: 'Original select',
+      content: [
+        {
+          id: 4565,
+          content: 'Original option 1',
+          selected: false
+        },
+        {
+          id: 1212,
+          content: 'Original option 2',
+          selected: true
+        }
+      ]
+    }
+  ]
+});
+```
+
+### 3.1.3.- search
+
+The possible options are:
+
+· **inner**: Filter the options in the dw-filter element
+
+· **outer**: Return the string into the search input when you realize a **val** methods
+
+· ***If is not define this attribute no show the search input on the element template***
+
+### 3.1.4.- height
+
+· **auto**: The element is high as its contents.
+
+· **number + px / %**: A specific height in px or %. Example:
+```javascript
+height: '250px'
+```
+
+· *if you not specified the value will be 132px
+
+
+### 3.1.5.- config
+### 3.1.6.- data
+
+## 3.2.- Val
+```javascript
+$('#id').dwFilter('val');
+```
+Return the follow structure:
+```javascript
+{
+  search: '',
+  data: []
+}
+```
+the **search** is the string of the **outer** .
+
+The **data** property is specific by **type** as follow:
+
+· **chekbox**: Array of change ids in options
+```javascript
+{
+  search: '',
+  data: [23, 78, 44]
+}
+```
+
+· **selectChain**: Array of change ids in options
+```javascript
+{
+  search: "text",
+	data: [
+		{
+			name: 'The select',
+			content: 1212 // id sede
+		},
+		{
+			name: 'Supe select',
+			content: null // no id select
+		}
+	]
+}
+```
+
+## 3.3.- Destroy
+This methods empty the container div and remove class too.
+```javascript
+$('#id').dwFilter('destroy');
+```
+
+# 4.- Listeners
+
+When change any value that affects the the filter data the selected div when w-filter has instantiated update and trigger a change events that you can listen as follow:
+
+```javascript
+$('#id').on({
+  change: function(event){
+    var result = $('#id').data('result');
+    console.log("sample1 data: ", result);
+  }
+});
+```
+
+# 5.- Demo
+You can view a local demo installing the component and open /bower_components/dw-filter/**index.html** in your browser (localhost/your_rute).
+
+You must change the bower_components dependencies rutes as follow:
+
+```html
+<script src="../jquery/dist/jquery.min.js"></script>
+<script src="../underscore/underscore-min.js"></script>
+```
+
+# 6.- Possible problems
+### 6.1.- Don't show svg background-image:
+
+Confirm that your server are serving well the svg files, add to your ***.htacces*** the follow:
+```bash
+AddType image/svg+xml .svg .svgz
+```
---
+++ component/dw-filter.js
@@ -0,0 +1,321 @@
+var scripts = document.getElementsByTagName("script");
+var urlBase = scripts[scripts.length-1].src;
+urlBase = urlBase.replace('dw-filter.js', '');
+
+// dwFilter
+(function( $ ){
+  "use strict";
+
+    // Public methods
+    let api = {
+      init : function(options) {
+        const $el = $(this);
+        // deploy component structure
+        let deployment = new Promise(function(resolve, reject){
+          methods.deployComponent($el, options);
+          resolve();
+        });
+        deployment.then(function(){
+          methods.getTemplate($el, options);
+        });
+      },
+      destroy: function(){
+        const $el = $(this);
+        $el.empty();
+        $el.removeClass('dw-filter');
+      },
+      val: function($el){
+        (typeof $el === 'undefined' || $el === null ) ? $el = $(this) : null;
+        const type = $el.data('type');
+        // builds each modified object
+        switch(type) {
+          case 'checkbox':
+            return methods.valCheckbox($el);
+          case 'selectChain':
+            return methods.valSelectChain($el);
+          case undefined:
+            return 'No type defined in this $el data';
+          default:
+            return 'This type not exist';
+        }
+      }
+    };
+
+    // Private methods
+    let methods = {
+      deployComponent: function($el, options){
+        // convert the div into a dw-filter component
+        $el.addClass('dw-filter');
+      },
+      getTemplate: function($el, options){
+        $.get(urlBase + "templates/dw-filter.html", function( result ) {
+          let templateContent = result;
+          methods.setTemplate($el, templateContent, options);
+        });
+      },
+      setTemplate : function($el, templateContent, options){
+        let titleVal = '';
+        let template;
+        if (typeof options !== 'undefined') {
+          (typeof options.title === 'undefined') ? titleVal = '' : titleVal = options.title;
+          template = _.template(templateContent);
+          $el.html( template({titleVal: titleVal}) );
+        }else{
+          template = _.template(templateContent);
+          $el.html( template({titleVal: ''}) );
+          $el.find('content').css('display','none');
+        }
+
+        if (typeof options !== 'undefined') {
+          // inject options according to filter type
+          methods.setOptionTemplate($el, options);
+          // show search input
+          methods.showSearch($el, options);
+          // set height
+          (typeof options.height !== 'undefined') ? methods.setHeight($el, options) : null;
+          // init events
+          events.start($el, options);
+        }
+
+      },
+      setOptionTemplate: function($el, options){
+        switch(options.type) {
+          case 'checkbox':
+            methods.checkboxTemplate($el, options);
+            break;
+          case 'selectChain':
+            methods.selectChainTemplate($el, options);
+            break;
+          default:
+            console.log(options.type + ": is not a valid type, or not have a template.");
+            break;
+        }
+      },
+      checkboxTemplate: function($el, options){
+        $el.data({
+          type: options.type
+        });
+        const key = options.config.key_attr;
+        const value = options.config.value_attr;
+
+        $.get(urlBase + "templates/checkbox.html", function( result ) {
+          let template = _.template(result);
+          // load component template
+          options['data'].forEach(data => {
+            let contentHtml = template({
+              key: data[key],
+              value: data[value]
+            });
+            $el.find('.dw-options').append(contentHtml);
+          });
+
+          // events for checkboxes
+          events.checkboxes($el);
+        });
+      },
+      selectChainTemplate: function($el, options){
+        $el.data({
+          type: options.type
+        });
+        const key = options.config.key_attr;
+        const name = options.config.name_attr;
+        const value = options.config.value_attr;
+
+        $.get(urlBase + "templates/selectChain.html", function( result ) {
+          let template = _.template(result);
+          options['data'].forEach(data => {
+            let contentHtml = template({
+              key: data[key],
+              name: data[name],
+              value: data[value]
+            });
+            $el.find('.dw-options').append(contentHtml);
+          });
+
+          // events for selectChain
+          events.selectChain($el, options);
+        });
+      },
+      showSearch: function($el, options){
+        let $search = $el.find('.search input');
+        ( options.search == 'inner' || options.search == 'outer' ) ? $search.toggleClass('hide') : null;
+      },
+      setHeight: function($el, options){
+        let $options = $el.find('.dw-options');
+        let heightVal = options.height;
+        if(heightVal == 'auto'){
+          $options.css({
+            height: 'auto'
+          });
+        }else if(heightVal.indexOf('px') >= 0 || heightVal.indexOf('px') >= 0){
+          $options.css({
+            height: options.height
+          });
+        }
+      },
+      valCheckbox: function($el){
+        let result = {
+          search: '',
+          data: []
+        };
+        let $options = $el.find('.dw-options');
+        let $option = $options.find('.dw-option');
+        // options
+        $option.toArray().forEach(opt => {
+          const $opt = $(opt);
+          let $optInput = $opt.find('input');
+          ( $optInput.is(':checked') ) ? result.data.push($opt.data('value')) : null;
+        });
+
+        // outerSearch
+        let outerSearch = methods.getOuterSearch($el);
+        if(typeof outerSearch !== 'undefined'){
+          result.search = outerSearch;
+        }
+        // update $el data
+        $el.data("result", result);
+        methods.passResult($el);
+        return result;
+      },
+      valSelectChain: function($el){
+        let result = {
+          search: '',
+          data: []
+        };
+        let $options = $el.find('.dw-options');
+        let $option = $options.find('.dw-option');
+        // options
+        $option.toArray().forEach(opt => {
+          const $opt = $(opt);
+          let $optInput = $opt.find('select');
+
+          let content = function(){
+            if($optInput.val() == 'undefined' || $optInput.val() == 'none'){
+              return null;
+            }else{
+              return parseInt( $optInput.val() );
+            }
+          }();
+
+          result.data.push({
+            name: $opt.attr('title'),
+            content: content
+          });
+        });
+        // outerSearch
+        let outerSearch = methods.getOuterSearch($el);
+        (typeof outerSearch !== 'undefined') ? result.search = outerSearch : null;
+        // update $el data
+        $el.data("result", result);
+        this.passResult($el);
+        return result;
+      },
+      innerSearch: function($el, inputData, options){
+        methods.hideOptions($el, inputData, options);
+      },
+      outerSearch: function($el, inputData, options){
+        $el.data({
+          dataSearch: inputData
+        });
+      },
+      getOuterSearch: function($el){
+        let dataSearch = $el.data('dataSearch');
+        return dataSearch;
+      },
+      hideOptions: function($el, data, options){
+        let $option = $el.find('.dw-option').toArray();
+        $option.forEach(opt => {
+          const $opt = $(opt);
+          let temp = $opt.data('content');
+          temp = temp.toLowerCase();
+          data = data.toLowerCase();
+          ( temp.indexOf(data) != -1 ) ? $opt.show() : $opt.hide();
+        });
+      },
+      passResult: function($el){
+        $el.trigger('change');
+      }
+    };
+
+    // Events
+    var events = {
+      start: function($el, options){
+        events.toggleContent($el, options);
+        events.onSearch($el, options);
+      },
+      toggleContent: function($el, options){
+        let $header = $el.find('header');
+        let $content = $el.find('content');
+        let $icon = $el.find('.icon-toggle');
+        $header.on({
+          click: function(){
+            $content.slideToggle('fast', function(){
+              if( $icon.hasClass('open') ){
+                $icon.toggleClass('open');
+                $icon.toggleClass('close');
+              }else{
+                $icon.toggleClass('open');
+                $icon.toggleClass('close');
+              }
+            });
+          }
+        });
+      },
+      onSearch: function($el, options){
+        let $search = $el.find('.search input');
+        $search.on({
+          keyup: function(event){
+            var inputData = $search.val();
+            switch(options.search){
+              case 'inner':
+                methods.innerSearch($el, inputData, options);
+                break;
+              case 'outer':
+                methods.outerSearch($el, inputData, options);
+                break;
+            }
+            if(options.search == 'outer'){
+              api.val($el);
+            }
+          },
+          focus: function(event){
+            $search.removeClass('glass');
+          },
+          focusout: function(event){
+            ($search.val().length > 0) ? $search.removeClass('glass') : $search.addClass('glass');
+          }
+        });
+      },
+      checkboxes: function($el){
+        $el.find('input').on({
+          change: function(event){
+            event.preventDefault();
+            event.stopPropagation();
+            api.val($el);
+          }
+        });
+      },
+      selectChain: function($el){
+        $el.find('select').on({
+          change: function(event){
+            event.preventDefault();
+            event.stopPropagation();
+            api.val($el);
+          }
+        });
+      }
+    };
+
+    // jquery component stuff
+    $.fn.dwFilter = function(methodOrOptions) {
+        if ( api[methodOrOptions] ) {
+            return api[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
+            // Default to "init"
+            return api.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.dwFilter' );
+        }
+    };
+
+})( jQuery );
